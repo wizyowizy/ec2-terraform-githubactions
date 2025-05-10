b9fb03f951905d09e7721125c1eaebb392ae64fa
obiomaokorowu <austinobi983@gmail.com>
2025-05-10 08:55:21 +0100

    added two more ec2

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,19 +3,16 @@ output "private_key_pem_base64" {
   sensitive = true
 }
 
-
-output "instance_public_ip" {
-  value = aws_instance.ec2.nginx
+output "instance_public_ip_nginx" {
+  value = aws_instance.nginx.public_ip
 }
 
-output "instance_public_ip" {
-  value = aws_instance.ec2.apache
+output "instance_public_ip_apache" {
+  value = aws_instance.apache.public_ip
 }
 
-output "instance_public_ip" {
-  value = aws_instance.ec2.mysql
+output "instance_public_ip_mysql" {
+  value = aws_instance.mysql.public_ip
 }
 
-#output "ubuntu_ami_id" {
-#  value = data.aws_ami.ubuntu.id
-#}
+
