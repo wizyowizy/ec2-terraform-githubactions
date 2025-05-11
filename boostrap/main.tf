a8df19fc0837ebea9c518a16b22666435b5f4d23
wizyowizy <wizyowizy@yahoo.com>
2025-05-11 12:45:05 -0400

    Update main.tf
    main.tf corrections

--- boostrap/main.tf
+++ boostrap/main.tf
@@ -21,17 +21,3 @@ resource "aws_s3_bucket_versioning" "tf_state_versioning" {
 
 
 
-##bootstrap/variables.tf
-
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Name of the bucket for storing tfstate"
-}
-
-
-###bootstrap/terraform.tfvars
-
-bucket_name = "realcloud-tfstate-bucket-001"
